ad30fbf0a9241e3d1c40ffbd1b1522a59d87c7a0
arshad1416 <arshad14@gmail.com>
2019-05-22 15:48:45 -0400

    Created a main js and scss file for future components.

---
+++ client/src/components/main/main.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import warehouses from '../warehouse/warehouse'
+import inventory from '../inventory/inventory';
+import './main.scss';
+import Axios from 'axios';
+const url = `http://localhost:8080`;
+
+class Main extends Component {
+    render(){
+        return(
+            <>
+
+            </>
+        );
+    }
+}
+
+export default Main;
