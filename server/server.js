7a8631bfd549a452069b8a27d6f576988c7bcaaa
arshad1416 <arshad14@gmail.com>
2019-05-22 15:00:23 -0400

    Just changed the folder name to lower case.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const instockroutes = require('./Routes/instockroutes');
+const instockroutes = require('./routes/instockroutes');
 const app = express();
 const port = 8080;
 const url = `http://localhost:8080`;
