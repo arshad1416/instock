0b4a8edbebb11ba2b9a8a50fe7249237bdf9a3a5
Benjamin MacDonald <ben_macdonald@hotmail.ca>
2019-05-23 15:58:17 -0400

    Fixed the inventory.

--- client/src/components/inventory/inventory.js
+++ client/src/components/inventory/inventory.js
@@ -2,9 +2,18 @@ import React, {Component} from 'react';
 import './inventory.scss';
 
 class Inventory extends Component {
-    render() {
-        return (
-
+            componentDidMount(){
+                axios.get('http://localhost:8080/inventory').then(res => {
+                    this.setState(() => ({inventory: res.data})); 
+                })};
+            
+                render() {
+                    return this.state.map(item =>(
+                        <div>
+                            <h2>Inventory</h2>
+                            <input type="text" placeholder="Search"/>
+                            <p>{item.}</p>
+                        </div>
         );
     }
 }
