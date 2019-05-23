e277418b153a77a39c0faf956676349256e9634e
arshad1416 <arshad14@gmail.com>
2019-05-23 11:58:20 -0400

    Removed test lines.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const inStockRoutes = require('./routes/instockroutes');
-const getInvItem = require('./getInvItemId/getInvItemId');
 const app = express();
 const port = 8080;
 const host = `http://localhost:${port}`;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.use('/inventory', getInvItem);
 
 app.use('/warehouses', inStockRoutes);
 app.use('/inventory', inStockRoutes);
