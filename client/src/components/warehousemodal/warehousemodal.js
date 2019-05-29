45d963ef6d622007adc061c7717a67a64546be96
arshad1416 <arshad14@gmail.com>
2019-05-29 15:24:50 -0400

    Deleted lines of code for Modal to work from past trials.

--- client/src/components/warehousemodal/warehousemodal.js
+++ client/src/components/warehousemodal/warehousemodal.js
@@ -1,29 +1,13 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import '../warehousemodal/warehousemodal.scss';
 
 class Warehousemodal extends Component {
-
-    PropTypes = {
-        onClose: PropTypes.func.isRequired,
-        show: PropTypes.bool,
-        children: PropTypes.node
-    };
 
     render() {
 
         if(!this.props.show) {
             return null;
         }
-
-        // const backdropStyle = {
-        //     position: 'fixed',
-        //     top: 0,
-        //     bottom: 0,
-        //     left: 0,
-        //     right: 0,
-        //     backgroundColor: 'rgba(0,0,0,0.3)',
-        //     padding: 50
-        // };
 
         return (
             <main className="warehouseModal warehouseModal__backDrop">
