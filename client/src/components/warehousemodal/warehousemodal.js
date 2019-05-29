eaa6f1496929f9c5fbec24bb51283a7bfec90131
arshad1416 <arshad14@gmail.com>
2019-05-29 10:29:54 -0400

    Trying out different codes for modal

--- client/src/components/warehousemodal/warehousemodal.js
+++ client/src/components/warehousemodal/warehousemodal.js
@@ -1,30 +1,49 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 class Warehousemodal extends Component {
 
-    state = { show: false };
+    PropTypes = {
+        onClose: PropTypes.func.isRequired,
+        show: PropTypes.bool,
+        children: PropTypes.node
+    };
 
-    
     render() {
+
+        if(!this.props.show) {
+            return null;
+        }
+
+        // const backdropStyle = {
+        //     position: 'fixed',
+        //     top: 0,
+        //     bottom: 0,
+        //     left: 0,
+        //     right: 0,
+        //     backgroundColor: 'rgba(0,0,0,0.3)',
+        //     padding: 50
+        // };
+
         return (
-            <main className="warehouseModal">
+            <main className="warehouseModal warehouseModal__backDrop">
                 <h1 className="warehouseModal__title">
                     Add New
                 </h1>
                 <section className="warehouseModal__spacing warehouseModal__warehouse">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="warehouse">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="warehouse">
                         warehouse
                     </h2>
                     <input type="text" className="warehouseModal__field" name="warehouse" placeholder="Name & ID" required />
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__address">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="address">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="address">
                         address
                     </h2>
                     <input type="text" className="warehouseModal__field" name="address" placeholder="Enter Address" required />
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__location">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="location">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="location">
                         location
                     </h2>
                     <select className="warehouseModal__field warehouseModal__dropDown" name="location">
@@ -36,31 +55,31 @@ class Warehousemodal extends Component {
                     </select>
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__contactName">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="contactName">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="contactName">
                         contact name
                     </h2>
                     <input type="text" className="warehouseModal__field" name="contactName" placeholder="Enter Name" required/>
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__position">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="position">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="position">
                         position
                     </h2>
                     <input type="text" className="warehouseModal__field" name="position" placeholder="Enter Position" required />
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__phoneNumber">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="phoneNumber">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="phoneNumber">
                         phone number
                     </h2>
                     <input type="tel" className="warehouseModal__field" name="phoneNumber" placeholder="(000) 000 - 0000" required />
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__email">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="email">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="email">
                         email
                     </h2>
                     <input type="email" className="warehouseModal__field" name="email" placeholder="email@instock.com" required />
                 </section>
                 <section className="warehouseModal__spacing warehouseModal__itemDescription">
-                    <h2 className="warehouseModal__fieldTitle" HtmlFor="itemDescription">
+                    <h2 className="warehouseModal__fieldTitle" htmlFor="itemDescription">
                         item description
                     </h2>
                     <textarea type="text" className="warehouseModal__field warehouseModal__textArea" name="itemDescription" placeholder="Use commas to separate each category" required />
@@ -69,7 +88,7 @@ class Warehousemodal extends Component {
                     <button className="warehouseModal__buttonSave">
                         save
                     </button>
-                    <button className="warehouseModal__buttonCancel">
+                    <button className="warehouseModal__buttonCancel" onClick={this.props.onClose}>
                         cancel
                     </button>
                 </section>
